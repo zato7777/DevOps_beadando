320cbd31aa63cf576e5543ad1c1fcb8e981d6841
Zalán Hajdú <h158325@stud.u-szeged.hu>
2025-12-04 20:58:45 +0100

    Prometheus, Grafana hozzáadása a terraformhoz

---
+++ terraform/grafana.tf
@@ -0,0 +1,81 @@
+resource "kubernetes_persistent_volume_claim" "grafana_pvc" {
+    metadata {
+        name = "grafana-pvc"
+    }
+    spec {
+        access_modes = ["ReadWriteOnce"]
+        resources {
+            requests = {
+                storage = "1Gi"
+            }
+        }
+    }
+}
+
+resource "kubernetes_deployment" "grafana" {
+    metadata {
+        name = "grafana-deployment"
+        labels = {
+            app = "grafana"
+        }
+    }
+
+    spec {
+        replicas = 1
+        selector {
+            match_labels = {
+                app = "grafana"
+            }
+        }
+        template {
+            metadata {
+                labels = {
+                    app = "grafana"
+                }
+            }
+            spec {
+                security_context {
+                    fs_group    = 472
+                    run_as_user = 472
+                }
+
+                container {
+                    name  = "grafana"
+                    image = "grafana/grafana:latest"
+
+                    port {
+                        container_port = 3000
+                    }
+
+                    volume_mount {
+                        mount_path = "/var/lib/grafana"
+                        name       = "grafana-storage"
+                    }
+                }
+
+                volume {
+                    name = "grafana-storage"
+                    persistent_volume_claim {
+                        claim_name = "grafana-pvc"
+                    }
+                }
+            }
+        }
+    }
+}
+
+resource "kubernetes_service" "grafana" {
+    metadata {
+        name = "grafana-service"
+    }
+    spec {
+        selector = {
+            app = "grafana"
+        }
+        port {
+            port        = 3000
+            target_port = 3000
+        }
+        type = "NodePort"
+    }
+}
---
+++ terraform/prometheus.tf
@@ -0,0 +1,86 @@
+resource "kubernetes_config_map" "prometheus_config" {
+    metadata {
+        name = "prometheus-config"
+    }
+
+    data = {
+        "prometheus.yml" = <<EOF
+        global:
+            scrape_interval: 10s
+
+        scrape_configs:
+            - job_name: 'prometheus'
+                static_configs:
+                    - targets: ['localhost:9090']
+
+            - job_name: 'backend-nodejs'
+                metrics_path: '/metrics'
+                static_configs:
+                    - targets: ['backend-service:5000']
+EOF
+  }
+}
+
+resource "kubernetes_deployment" "prometheus" {
+    metadata {
+        name = "prometheus-deployment"
+        labels = {
+            app = "prometheus"
+        }
+    }
+
+    spec {
+        replicas = 1
+        selector {
+            match_labels = {
+                app = "prometheus"
+            }
+        }
+        template {
+            metadata {
+                labels = {
+                    app = "prometheus"
+                }
+            }
+
+            spec {
+                container {
+                    name  = "prometheus"
+                    image = "prom/prometheus:latest"
+                    args = ["--config.file=/etc/prometheus/prometheus.yml"]
+          
+                    port {
+                        container_port = 9090
+                    }
+
+                    volume_mount {
+                        name = "config-volume"
+                        mount_path = "/etc/prometheus/"
+                    }
+                }
+                volume {
+                    name = "config-volume"
+                    config_map {
+                        name = "prometheus-config"
+                    }
+                }
+            }
+        }
+    }
+}
+
+resource "kubernetes_service" "prometheus" {
+    metadata {
+        name = "prometheus-service"
+    }
+    spec {
+        selector = {
+            app = "prometheus"
+        }
+        port {
+            port        = 9090
+            target_port = 9090
+        }
+    type = "NodePort"
+  }
+}
